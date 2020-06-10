f5665c5f6f1e82b333e6f1f438f109edd7da176f
hfog2066 <hfog2000@yahoo.com>
2020-06-10 18:20:09 -0500

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Write a line to the screen
+ *
+ *Description: this function show a text line
+ *"Programing is like building a multilingual puzzle"
+ *using puts function
+ *Return: 0 at the end of the routine
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
