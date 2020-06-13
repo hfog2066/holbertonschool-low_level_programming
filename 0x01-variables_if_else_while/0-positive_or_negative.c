5e363838230837204605d1bc5be9de3de7419d2d
hfog2066 <hfog2000@yahoo.com>
2020-06-12 19:31:08 -0500

    Positive anything is better negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,16 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive", n);
+{
+printf("%d is positive\n", n);
+}
 else if (n < 0)
-printf("%d is negative", n);
+{
+printf("%d is negative\n", n);
+}
 else
-printf("%d is zero", n);
-putchar('\n');
+{
+printf("%d is zero\n", n);
+}
 return (0);
 }
