36ff234cdd9490f84fb7e4bf4748c3049a3be661
hfog2066 <hfog2000@yahoo.com>
2020-06-16 16:39:06 -0500

    Alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,13 @@
  *
  *Return: 0.
  */
+
 void print_alphabet_x10(void)
 {
-char *c = "abcdefghijklmnopqrstuvwxyz";
-char *t = c;
 int i;
-for (i = 0; i < 10; i++)
+for (i = 97; i <= 122; i++)
 {
-while (*c)
-_putchar(*(c++));
+_putchar(i);
 _putchar('\n');
-c = t;
 }
 }
