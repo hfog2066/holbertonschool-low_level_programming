50a908210869e8915ce319f10274ad6a11beeb4c
hfog2066 <hfog2000@yahoo.com>
2020-06-16 15:33:22 -0500

    Headers 0x02

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-	     
+
+#endif
