16511df2ac37c74f2a3d452a48eaf8788a949158
hfog2066 <hfog2000@yahoo.com>
2020-06-16 17:13:34 -0500

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <holberton.h>
+/**
+ *main - show lowercase character.
+ *
+ *Description: checks for lowercase
+ *character.
+ *
+ *Return: 0 
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
