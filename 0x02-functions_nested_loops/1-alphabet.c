3627b028b599964807d0c59636f594f729e1cb01
hfog2066 <hfog2000@yahoo.com>
2020-06-16 09:55:05 -0500

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *main - show alphabet
+ *
+ *Description: print alphabet in lowercase
+ *
+ *Return: 0 at the end of the routine
+ */
+int print_alphabet(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
