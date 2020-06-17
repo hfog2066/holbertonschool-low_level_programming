d0d42b53be5251ec5e22d4a5b1ffdf77ac3c8de4
hfog2066 <hfog2000@yahoo.com>
2020-06-16 22:37:23 -0500

    C-Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *main - show integers
+ *Description: print positive or negative numbers
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
