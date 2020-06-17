70f478798abb2a044d33d0a9f27500673e55e41a
hfog2066 <hfog2000@yahoo.com>
2020-06-17 10:35:42 -0500

    holbeton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,17 +1,11 @@
-#ifndef _holberton_h_
-#define _holberton_h_
+#ifndef holberton_h
+#define holberton_h
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
