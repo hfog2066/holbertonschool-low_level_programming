ff7809b8c622fd0a0dd3aceec662c79ea74515bf
hfog2066 <hfog2000@yahoo.com>
2020-06-17 11:06:51 -0500

    Leap year

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *print_remaining_days - print how many days are
+ *
+ * @month: month in number format
+ * @day: day of the month
+ * @year: year
+ * Return: void
+ */
+void print_remaining_days(int month, int day, int year)
+{
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+if (month >= 2 && day >= 60)
+{
+day++;
+}
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
+else
+}
+if (month == 2 && day >= 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
+}
+}
