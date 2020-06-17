6b73adea3f596bb84ae9b038b0ad56bb1c0a49c8
hfog2066 <hfog2000@yahoo.com>
2020-06-16 19:41:44 -0500

    There is no such...

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *main - show absolute value
+ *
+ *Description: prints absolute value of
+ *a integer.
+ *
+ *Return: 0
+ */
+int _abs(int);
+{
+if (n <= 0)
+return (n - (n * 2));
+else
+return (n);
+}
