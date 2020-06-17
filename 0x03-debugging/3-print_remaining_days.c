e109c99787295c51a707e4d3fe86aee888fbc563
hfog2066 <hfog2000@yahoo.com>
2020-06-17 13:47:30 -0500

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,7 +23,11 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", day);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
