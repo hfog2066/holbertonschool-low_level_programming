52b51cccc063f28653a3158df0893ed3009253eb
hfog2066 <hfog2000@yahoo.com>
2020-06-17 11:54:21 -0500

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *print_remaining_days - print how many days are
- *
+ *print_remaining_days - takes a date and prints how many days are
+ *left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of the month
  * @year: year
@@ -10,7 +10,7 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 {
 if (month > 2 && day >= 60)
 {
