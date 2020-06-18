6ce6e37ae2925e2fc4e2eb00ab5965bec7cfd4c5
hfog2066 <hfog2000@yahoo.com>
2020-06-18 18:24:34 -0500

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/*
+ * main - print prime numbers
+ *
+ *Return: 0
+ *
+ */
+int main(void)
+{
+	long int i;
+	long int n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			i++;
+		}
+	}
+	printf("%ld\n", i);
+	return (0);
+}
