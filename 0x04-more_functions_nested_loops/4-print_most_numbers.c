96782b3f5dd41e060f4fbcce843f0d91fca39d80
hfog2066 <hfog2000@yahoo.com>
2020-06-18 15:21:19 -0500

    4.I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers - prints numbers 0 to 9.
+ *
+ *Return: void
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
