30aa80f954473a334ff43fc58190199c6130d208
hfog2066 <hfog2000@yahoo.com>
2020-06-18 14:23:37 -0500

    1.isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _isdigit - print 1 if c is a digit.
+ *
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
