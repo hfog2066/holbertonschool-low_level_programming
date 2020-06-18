eec646904c9a97758a488dc1dc716aeea0db8b74
hfog2066 <hfog2000@yahoo.com>
2020-06-18 10:26:04 -0500

    holberton.h

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+
+#endif /*HOLBERTON_H */
+
+
