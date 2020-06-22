34c8f78104417cbe6289fbb1b3de44b265df43e1
hfog2066 <hfog2000@yahoo.com>
2020-06-22 18:58:15 -0500

    holberton.h prototype

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,3 +11,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+
+#endif 
