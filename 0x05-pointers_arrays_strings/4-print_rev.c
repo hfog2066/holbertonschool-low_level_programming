7a96528f92331180211df161e5938f585a1ff4d8
hfog2066 <hfog2000@yahoo.com>
2020-06-22 19:46:35 -0500

    I can only go one way. I've not got a reverse gear.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *print_rev - print string in reverse.
+ *@s: input string.
+ *Return: no return.
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
