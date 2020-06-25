c944ec4a2e912219456ec003cf407c3bc972c6d5
hfog2066 <hfog2000@yahoo.com>
2020-06-25 16:10:42 -0500

    Expect the best. Prepare for the worst...

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
 char *cap_string(char *s)
 {
 	int a = 0, i;
-	int tab_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + a) >= 97 && *(s + a) <= 122)
 		*(s + a) = *(s + a) - 32;
@@ -17,7 +17,7 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + a) == tab_words[i])
+			if (*(s + a) == sep_words[i])
 			{
 				if ((*(s + (a + 1)) >= 97) && (*(s + (a + 1)) <= 122))
 					*(s + (a + 1)) = *(s + (a + 1)) - 32;
