4ad5a5cd814fd7338f80838fb5e90b23dc0b1db6
hfog2066 <hfog2000@yahoo.com>
2020-06-25 09:40:07 -0500

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *_strcat -concatenates strings
+ *@dest: destination.
+ *@src: source.
+ *Return: the pointer to dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (*(dest + a) != '\0')
+	{
+		a++;
+	}
+
+	while (b >= 0)
+	{
+		*(dest + a) = *(src + b);
+		if (*(src + b) == '\0')
+			break;
+		a++;
+		b++;
+	}
+	return (dest);
+}
