365f41d924fd9e4165bc2fb5df621a0809f23f24
hfog2066 <hfog2000@yahoo.com>
2020-06-25 11:42:23 -0500

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_strncpy - copy a string.
+ *@dest: destination.
+ *@src: source.
+ *@n: amount of bytes used by src.
+ *Return: the pointer to dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
