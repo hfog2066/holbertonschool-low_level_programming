fa2f536cd71833cf25aa97b09ed682fdfa51121e
hfog2066 <hfog2000@yahoo.com>
2020-07-13 18:09:52 -0500

    0. Django.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *dog_t - Typedef struct dog.
+ */
+
+typedef struct dog dog_t;
+
+/**
+ *struct dog - stores information dog
+ *@name: name dog
+ *@age: age dog
+ *@owner: owner dog.
+ *
+ *Description: struct called "dog" store name, age, and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
