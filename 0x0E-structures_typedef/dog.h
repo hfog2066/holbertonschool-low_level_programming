9f14918dcb2d7e720f7f26fb72a65da451312275
hfog2066 <hfog2000@yahoo.com>
2020-07-13 18:38:42 -0500

    0. Django.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 /**
  *dog_t - Typedef struct dog.
  */
@@ -33,5 +27,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _putchar(char c);
 
 #endif
