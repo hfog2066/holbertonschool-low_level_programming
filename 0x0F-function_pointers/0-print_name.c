a3aa5182b8f025b22b84bd5f3e151257f57b8145
hfog2066 <hfog2000@yahoo.com>
2020-07-15 16:35:11 -0500

    0. What's my name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - print name.
+ *@name: input name.
+ *@f: function pointer.
+ *
+ *Return: 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
