f8db72d2e13b45c8fb4a4655b3a0084e3485d39e
hfog2066 <hfog2000@yahoo.com>
2020-07-18 15:40:30 -0500

    3. To be is a to be the value of a variable.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,51 +1,93 @@
 #include "variadic_functions.h"
+
 /**
- *print_all - prints anything.
- *@format: list arguments.
+ *print_char - c file
+ *@valist: argument
+ *
+ *Return: 0.
+ */
+void print_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+/**
+ *print_number - c file
+ *@valist: argument.
+ *
+ *Return:  0.
+ */
+void print_number(va_list valist)
+{
+	printf("%i", va_arg(valist, int));
+}
+/**
+ *print_float - c file
+ *@valist: argument.
+ *
+ *Return: 0.
+ */
+void print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+/**
+ *print_string - template c file
+ *@valist: argument.
+ *
+ *Return: 0.
+ */
+void print_string(va_list valist)
+{
+	char *string = va_arg(valist, char *);
+
+	if (!string)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", string);
+}
+/**
+ *print_all - print all formats.
+ *@format: character format.
  *
  *Return: 0.
  */
 void print_all(const char * const format, ...)
 {
+	op_t list_op[] = {
+		{"c", print_char},
+		{"i", print_number},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	char *t_format = (char *) format;
+	char *separator = "";
 	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	int i = 0;
 
 	va_start(valist, format);
-	while (format && format[i])
-	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && c)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s", str);
-			break;
 
-		} i++;
+	while (format && *t_format)
+	{
+		i = 0;
+
+		while (list_op[i].op)
+		{
+			if (list_op[i].op[0] == *t_format)
+			{
+				printf("%s", separator);
+				list_op[i].fun(valist);
+				separator = ", ";
+			}
+			i++;
+		}
+		t_format++;
 	}
-	printf("\n"), va_end(valist);
+
+	va_end(valist);
+	printf("\n");
 }
