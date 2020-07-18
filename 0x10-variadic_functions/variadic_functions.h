40eae5bf1471b90f520b1bdd20c6435f11c5b670
hfog2066 <hfog2000@yahoo.com>
2020-07-18 15:43:54 -0500

    variadic_functions.h prototypes.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,4 +10,22 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
+/**
+ *struct op - struct operator
+ *@op: char.
+ *@fun: function print.
+ *
+ *Description: define struct type.
+ */
+typedef struct op
+{
+	char *op;
+	void (*fun)(va_list);
+} op_t;
+
+void print_number(va_list);
+void print_string(va_list);
+void print_float(va_list);
+void print_char(va_list);
+
 #endif
