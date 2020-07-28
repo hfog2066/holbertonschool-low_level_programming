ad5a9b7ed4f5061c59a25e42cbc8ec56928d8c76
hfog2066 <hfog2000@yahoo.com>
2020-07-28 08:25:06 -0500

    0. Print list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *print_listint - prints all the elements of a listint_t list.
+ *@h: list head.
+ *Return: number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t anodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		anodes++;
+	}
+	return (anodes);
+}
