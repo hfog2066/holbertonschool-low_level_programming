de3f14ae86ae7575fd9beddfe025198857ea9f2c
hfog2066 <hfog2000@yahoo.com>
2020-08-22 12:41:45 -0500

    In computer class...

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - prints Fiobinacci numbers
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int c;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+
+	printf("%d, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}
