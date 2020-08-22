3b9e44b7d33290696dafd16133d5ae12dfda128b
hfog2066 <hfog2000@yahoo.com>
2020-08-22 12:30:51 -0500

    Nature made...

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints natural numbers
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
