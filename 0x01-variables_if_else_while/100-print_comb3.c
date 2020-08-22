df366611a64dc70a461bf403c864b8ef66addf5b
hfog2066 <hfog2000@yahoo.com>
2020-08-22 09:15:02 -0500

    11.Inventing is a combination...

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main - prints numbers 00 to 89
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	j = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (j != 1 && e < i)
+			{
+
+				putchar(j);
+				putchar(i);
+				if (i == 57 && j == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(',');
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
