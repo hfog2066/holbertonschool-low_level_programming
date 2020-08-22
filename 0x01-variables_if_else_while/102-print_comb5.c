3bc08ac9b19486c46eb5e1dc02052faace26e60f
hfog2066 <hfog2000@yahoo.com>
2020-08-22 11:27:42 -0500

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
 
-/*
- *main - prints numbers 00 to 99
+/**
+ * main - Prints numbers 00 to 99.
  *
- *Return: Always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-	int i, j, k, h, op1, op2;
+int i, e, g, h, op1, op2;
 
-	i = j = k = h = 48;
-	while (h < 58)
+i = e = g = h = 48;
+while (h < 58)
+{
+	g = 48;
+	while (g < 58)
 	{
-		k = 48;
-		while (k < 58)
+		e = 48;
+		while (e < 58)
 		{
-			j = 48;
-			while (j < 58)
+			i = 48;
+			while (i < 58)
 			{
-				i = 48;
-				while (i < 58)
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
 				{
-					op1 = (h * 10) + k;
-					op2 = (j * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(h);
-						putchar(k);
-						putchar(' ');
-						putchar(j);
-						putchar(i);
-						if (h == 57 && k == 56 && j == 57 && i == 57)
-							break;
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
 						putchar(',');
 						putchar(' ');
-					}
-					i++;
 				}
-				j++;
+				i++;
 			}
-			k++;
+			e++;
 		}
-		h++;
+		g++;
 	}
-	putchar('\n');
-	return (0);
+	h++;
+}
+putchar('\n');
+return (0);
 }
