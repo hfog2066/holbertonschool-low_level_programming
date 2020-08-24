f78c4d0bd28f6f08489903f9e8d004a21981c839
hfog2066 <hfog2000@yahoo.com>
2020-08-24 12:39:16 -0500

    It is the addition of strangeness to beauty...

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -24,7 +24,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
-	r[bg +1] = '\0';
+	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
