18bf0c6cf3a612eacd8544ec233db0721a4a6178
hfog2066 <hfog2000@yahoo.com>
2020-10-02 07:00:36 -0500

    3. >>> ht['betty'] = 'holberton'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -62,7 +62,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	return (1);
-}#include "hash_tables.h"
+}
+
+#include "hash_tables.h"
 
 /**
  * add_n_hash - adds a node at the beginning of a hash at a given index
